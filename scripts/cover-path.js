522ba660f244d29d9379c32d024d3094cb2141ea
1113652622-svg <1113652622@qq.com>
2026-06-03 00:50:15 +0800

    Add separate header background support

--- scripts/cover-path.js
+++ scripts/cover-path.js
@@ -32,6 +32,18 @@ function randomCover(count, images) {
   return count > 1 ? Array.from({ length: count }, select) : select();
 }
 
+function headerCover(item) {
+  if (item.background) {
+    return normalizeCoverPath(item.background);
+  }
+
+  if (item.cover) {
+    return normalizeCoverPath(item.cover);
+  }
+
+  return randomCover(1, hexo.theme.config.image_list);
+}
+
 hexo.extend.filter.register("before_generate", () => {
   hexo.extend.helper.register("_image_url", normalizeCoverPath);
 
@@ -48,15 +60,23 @@ hexo.extend.filter.register("before_generate", () => {
   });
 
   hexo.extend.helper.register("_cover_index", function(item) {
-    if (item.cover) {
-      return normalizeCoverPath(item.cover);
-    }
-
-    if (item.photos && item.photos.length > 0) {
-      return normalizeCoverPath(item.photos[0]);
+    if (!item.__index) {
+      return headerCover(item);
     }
 
     const { image_list: images, index_images: indexImages } = hexo.theme.config;
     return randomCover(6, indexImages && indexImages.length ? indexImages : images);
   });
+});
+
+hexo.extend.filter.register("template_locals", (locals) => {
+  if (!locals.page.__index && locals.theme.homeConfig?.fixedCover) {
+    locals.theme = {
+      ...locals.theme,
+      homeConfig: {
+        ...locals.theme.homeConfig,
+        fixedCover: ""
+      }
+    };
+  }
 });
